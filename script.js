918e28bba1e822fc691395f3f1f4604ff2513c39
MarcoAntolini <marco.antolini2001@gmail.com>
2022-12-06 02:12:24 +0100

    links

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function websiteVisits(response) {
     document.querySelector("#visits").textContent = response.value;
 }
+
+// https://www.youtube.com/watch?v=OhCzEjXvY9A
+// https://www.youtube.com/watch?v=mxHoPYFsTuk
+// https://www.youtube.com/watch?v=zNXMkjp2Sv8
+// https://www.youtube.com/watch?v=hAnv1NEE7j8
